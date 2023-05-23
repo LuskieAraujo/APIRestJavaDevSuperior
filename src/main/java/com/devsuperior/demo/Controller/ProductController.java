c182cc891d0b0b03b2802dfed31957dfb2348afb
Lucas Araújo <araujo_depaula@outlook.com>
2023-05-23 11:18:21 -0300

    Finalização do projeto

--- src/main/java/com/devsuperior/demo/Controller/ProductController.java
+++ src/main/java/com/devsuperior/demo/Controller/ProductController.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
     @GetMapping
-    public Department listarDepartamentos(){
+    public List<Product> listarProdutos(){
         Department d1 = new Department(1, "Tech");
         Department d2 = new Department(2, "Pet");
 
         Product p1 = new Product(1, "MacBook Pro", 4000.00, d1);
         Product p2 = new Product(2, "PC Gamer", 5000.00, d1);
-        return d1;
+        Product p3 = new Product(3, "Casinha pet", 300, d2);
+
+        return Arrays.asList(p1,p2,p3);
     }
 }
